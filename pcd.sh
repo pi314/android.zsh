bbbb03e56f686b390b1a7515b28b1f3abcdbe45e
Cychih <michael66230@gmail.com>
2015-12-03 00:58:50 +0800

    refine _pcd() and pcd(), more like path completion now

--- pcd.sh
+++ pcd.sh
@@ -4,6 +4,10 @@ pcd () {
     else
         relative_dir="$1"
     fi
-    export PHONE_PWD=$(adb shell "cd $PHONE_PWD; cd $relative_dir; pwd" | tr -d '\r')
+    _old_phone_pwd="$PHONE_PWD"
+    export PHONE_PWD=$(adb shell "cd $PHONE_PWD && cd $relative_dir 2>/dev/null; pwd" | tr -d '\r')
     echo $PHONE_PWD
+    if [ "$_old_phone_pwd" = "$PHONE_PWD" ]; then
+        false
+    fi
 }
